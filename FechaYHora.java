73d31fe9fd50cde4be27b2ae9c3d78dd2e35f17b
Yav <yavieron133@gmail.com>
2017-11-10 12:33:22 +0100

    Creado método que devuelve la fecha y la hora

--- FechaYHora.java
+++ FechaYHora.java
@@ -13,6 +13,43 @@ public class FechaYHora {
         minuto = 0;
         hora = 0;
     }
-        
+    
+    public String getFechaYHora(){
+        String fechaHora = "";
+        if(dia < 10) {
+            fechaHora = "0" + dia;
+        }
+        else {
+            fechaHora = "" + dia;
+        }
+        fechaHora += "-";
+        if(mes < 10) {
+            fechaHora += "0" + mes;
+        }
+        else {
+            fechaHora += "" + mes;
+        }
+        fechaHora += "-";
+        if(ano < 10) {
+            fechaHora += "0" + ano;
+        }
+        else {
+            fechaHora += "" + ano;
+        }
+        fechaHora += " ";
+        if(hora < 10) {
+            fechaHora += "0" + hora;
+        }
+        else {
+            fechaHora += "" + hora;
+        }
+        fechaHora += ":";
+        if(minuto < 10) {
+            fechaHora += "0" + minuto;
+        }
+        else {
+            fechaHora += "" + minuto;
+        }
+        return fechaHora;
+    }    
 }
-
