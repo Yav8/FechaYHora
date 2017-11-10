109a7c610eea0710c929a877a5a7d2adf65ab00a
Yav <yavieron133@gmail.com>
2017-11-10 12:20:33 +0100

    Creada clase FechaYHora y constructor

---
+++ FechaYHora.java
@@ -0,0 +1,18 @@
+
+public class FechaYHora {
+    private int dia;
+    private int mes;
+    private int ano;
+    private int minuto;
+    private int hora;
+
+    public FechaYHora() {
+        dia = 1;
+        mes = 1;
+        ano = 1;
+        minuto = 0;
+        hora = 0;
+    }
+        
+}
+
